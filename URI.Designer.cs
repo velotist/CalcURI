b4c000351cbb02756e46e36c98f3c01f25d4f253
Velotist <velotist@aikq.de>
2018-12-16 19:57:40 +0100

    Added files

---
+++ URI.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace URI_Rechner_V1
+{
+    partial class URI
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(URI));
+            this.CmdBerechne = new System.Windows.Forms.Button();
+            this.CmdEnde = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CmdBerechne
+            // 
+            this.CmdBerechne.Location = new System.Drawing.Point(119, 290);
+            this.CmdBerechne.Name = "CmdBerechne";
+            this.CmdBerechne.Size = new System.Drawing.Size(137, 26);
+            this.CmdBerechne.TabIndex = 0;
+            this.CmdBerechne.Text = "Berechne";
+            this.CmdBerechne.UseVisualStyleBackColor = true;
+            this.CmdBerechne.Click += new System.EventHandler(this.CmdBerechne_Click);
+            // 
+            // CmdEnde
+            // 
+            this.CmdEnde.Location = new System.Drawing.Point(155, 340);
+            this.CmdEnde.Name = "CmdEnde";
+            this.CmdEnde.Size = new System.Drawing.Size(68, 43);
+            this.CmdEnde.TabIndex = 1;
+            this.CmdEnde.Text = "Ende";
+            this.CmdEnde.UseVisualStyleBackColor = true;
+            this.CmdEnde.Click += new System.EventHandler(this.CmdEnde_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DarkRed;
+            this.textBox1.Location = new System.Drawing.Point(119, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textBox2.Location = new System.Drawing.Point(119, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(137, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox3.Location = new System.Drawing.Point(119, 143);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(137, 26);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "U";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(288, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "R";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(292, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "I";
+            // 
+            // URI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(379, 405);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CmdEnde);
+            this.Controls.Add(this.CmdBerechne);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Georgia", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "URI";
+            this.Text = "URI Rechner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdBerechne;
+        private System.Windows.Forms.Button CmdEnde;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ URI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+// Namensraum (Paket) für meine Klasse URI_Rechner_V1
+namespace URI_Rechner_V1
+{
+    // Öffentliche Klasse mit teilweise nicht sichtbaren Dateien
+    public partial class URI : Form
+    {
+        public URI()
+        {
+            // Methode zum Erzeugen der Oberfläche
+            InitializeComponent();
+        }
+
+        // Methode wird nur für diese Klasse bereitgestellt, für den Button Berechne beim Daraufklicken, wurde automatisch generiert
+        private void CmdBerechne_Click(object sender, EventArgs e)
+        {
+
+        }
+        // Methode wird nur für diese Klasse bereitgestellt, für den Button Ende beim Daraufklicken, wurde automatisch generiert
+        private void CmdEnde_Click(object sender, EventArgs e)
+        {
+            // Methode zum Schließen des Formulars
+            Close();
+        }
+    }
+}
